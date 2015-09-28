968edfc64385397f1e4ab21ecf379ea00e58927e
lconeill <luke.c.oneill@gmail.com>
2015-09-27 22:14:09 -0400

    Slot Game
    -added roll button and smooth start to the rotation

--- Assets/Scripts/SlotGame.cs
+++ Assets/Scripts/SlotGame.cs
@@ -12,7 +12,7 @@ public class SlotGame : MonoBehaviour
 
     private Vector3 rotation = Vector3.zero;    //The rotation vector of the slot reel
     private string[] reelIcon = new string[7] {"Bell","Fruit Gum","Lemon","Cherry1","Grape","Orange","Cherry2"};
-    private bool continueRotation = true;       //Flag used to stop rotation when the button is pressed
+    private bool continueRotation = false;       //Flag used to stop rotation when the button is pressed
     private Vector3 updateRotation = Vector3.zero;  //Used to update the rotation vector of the slot reel after button is pressed
 
     private float currentLerpTime = 0;      //Variable that is updated to provide an argument to Math.Lerp
@@ -20,6 +20,10 @@ public class SlotGame : MonoBehaviour
     private float degreesPerIcon;           //Used to find out which icon we are on when the buttin is pressed
 
     public slotManager captureResult;
+
+    public bool startRoll = false;          //Flag used to start spinning the reels
+    private bool stopRoll = false;          //Flag used to stop spinning the reels
+    private float startRollLerp = 0;        
 
     //Initialize the slot reel starting rotation
     void Start()
@@ -32,13 +36,26 @@ public class SlotGame : MonoBehaviour
     // Otherwise call smoothStop
     void Update()
     {
-        if (continueRotation)
+        if (startRoll)
+        {
+            startRollLerp += Time.deltaTime;
+
+            if (startRollLerp > stopSpeed)
+            {
+                startRollLerp = stopSpeed;
+                startRoll = false;
+                continueRotation = true;
+            }
+            smoothStart();
+        }
+
+        else if (continueRotation)
         {
             rotation.x += rotationSpeed * Time.deltaTime;
             transform.eulerAngles = rotation;
         }
 
-        else
+        else if (stopRoll)
         {
             currentLerpTime += Time.deltaTime;
 
@@ -52,10 +69,17 @@ public class SlotGame : MonoBehaviour
         
     }
 
+    // This is called when the roll button is pressed
+    public void startSpinSwtich()
+    {
+        startRoll = true;
+    }
+
     // This should be called when the button is pressed
     public void stopSpin()
     {
         continueRotation = false;
+        stopRoll = true;
         slotButton.GetComponent<Button>().interactable = false;
 
         float revolutionsRemainder = rotation.x % 360;
@@ -74,6 +98,17 @@ public class SlotGame : MonoBehaviour
         float endRotation = System.Convert.ToSingle(rotation.x % 360 % degreesPerIcon + degreesPerIcon / 2);
 
         updateRotation.x = Mathf.Lerp(rotation.x, rotation.x - (360 + endRotation), perc);
+
+        transform.eulerAngles = updateRotation;
+    }
+
+    // Smoothly starts the slot reel rotation when the roll button is pressed
+    public void smoothStart()
+    {
+        float perc = startRollLerp / stopSpeed;
+        perc = 1 - Mathf.Cos(perc * Mathf.PI * 0.5f);
+
+        updateRotation.x = Mathf.Lerp(rotation.x, rotation.x - 360, perc);
 
         transform.eulerAngles = updateRotation;
     }
