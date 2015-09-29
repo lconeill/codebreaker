b6242f5da9eed4388da068f980dbcdae779c16f4
Jason Coelho <jason.coelho@me.com>
2015-09-28 22:29:15 -0400

    Initial Commit
    Added the scripts for Moving objects Wheel Logic and Touch Input Test.

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveScript : MonoBehaviour 
+{
+	// This Script handles the Movement of the tiles
+	// in gameplay. When a tile appears the player can
+	// swip or flick in the direction of the match area
+	// they are trying to get to and the tile will hold 
+	// at a specified max distacne even if the player
+	// continues to move their finger in a direction.
+	// if the player lifts their finger before reaching
+	// the match area the tile will jump back to its
+	// starting point.
+	
+	public float horizontal_limit = 1.0f;
+	public float vertical_limit = 1.0f;
+	public float drag_speed = 0.01f;
+	
+	Transform cached_transform;
+	Vector3 starting_pos;
+	
+	// Initializing the tiles position.
+	
+	void Start()
+	{
+		cached_transform = transform;
+		
+		starting_pos = cached_transform.position;
+	}
+	
+	
+	void Update () 
+	{
+		// Thourgh a switch statmet get the current 
+		// state of the Touch Input. If state is 
+		// Moved then call the DragObject function
+		// to move the tile, any othe rcase the object
+		// will not move.
+		
+		if(Input.touchCount == 1)
+		{
+			// Get the delta of when the player touches the 
+			// screen and what direction they are moving.
+			
+			Vector2 deltaPosition = Input.GetTouch (0).deltaPosition;
+			
+			switch(Input.GetTouch (0).phase)
+			{
+				case TouchPhase.Began:
+					break;
+				
+				case TouchPhase.Moved:
+					DragObject (deltaPosition);
+					break;
+				
+				case TouchPhase.Ended:
+					break;
+			}
+		}
+		
+		else
+		{
+			// When the player lifts their finger set
+			// the tiles positoin back to its starting point.
+			
+			cached_transform.position = starting_pos;
+		}
+
+	}
+	
+	// Move the tile by changing its position based on the 
+	// touch input of the player and limit the distance
+	// the tile can go in either directin with a clamp.
+	
+	void DragObject(Vector2 deltaPosition)
+	{
+		cached_transform.position = new Vector3(Mathf.Clamp((deltaPosition.x * drag_speed) + cached_transform.position.x,
+		                                                    starting_pos.x - horizontal_limit, starting_pos.x + horizontal_limit),
+		                                                    Mathf.Clamp((deltaPosition.y * drag_speed) + cached_transform.position.y,
+		                                                    starting_pos.y - vertical_limit, starting_pos.y + vertical_limit),
+		                                        			cached_transform.position.z);
+	}
+}
---
+++ Assets/Scripts/WheelLogic.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WheelLogic : MonoBehaviour 
+{
+	// This script is attached to Each match area
+	// The function of the script in its current state is to
+	// check for collision when a tile enters its collider.
+	// When a correct match is made the circular timer script is
+	// invoked and calls the functions to move the tiels
+	// and reset the timer.
+	
+	public GameObject circular_timer_ref;
+	
+	private CircularTimer circular_timer_Script;
+
+	void Start () 
+	{
+		circular_timer_Script = circular_timer_ref.GetComponent<CircularTimer>();
+	}
+	
+	// This fucntion is called when a trigger collider enters this objects collider.
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if(this.gameObject.tag == col.tag.Replace("Source", "Target"))
+		{
+			Debug.Log(col.tag);
+
+			circular_timer_Script.moveTile.moveTiles();
+			circular_timer_Script.Reset();
+		}
+		
+		else
+		{
+			Debug.Log("This is not a match!");
+		}
+	}
+}
