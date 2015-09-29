0fede734e65edac075f91248e099f052247613fd
lconeill <luke.c.oneill@gmail.com>
2015-09-28 22:56:21 -0400

    Jason Changes from Initial Commit
    Merging to my remote branch

---
+++ Assets/Scripts/TouchInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchInput : MonoBehaviour 
+{
+
+	// This is purly for testing with touch input.
+	// the script will display the ID, phase, 
+	// position etc on a GUI texture overlaying
+	// on the screen.
+	
+	void OnGUI()
+	{
+		foreach(Touch touch in Input.touches)
+		{
+			string message = "";
+			message += "ID: " + touch.fingerId + "\n";
+			message += "Phase: " + touch.phase.ToString() + "\n";
+			message += "TapCount: " + touch.tapCount + "\n";
+			message += "Pos X: " + touch.position.x + "\n";
+			message += "Pos Y: " + touch.position.y + "\n";
+			
+			int num = touch.fingerId;
+			GUI.Label(new Rect(0 + 130 * num, 0, 120, 100), message);
+		}
+	}
+}
