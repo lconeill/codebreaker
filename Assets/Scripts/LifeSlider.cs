b1392da4c33b8122aed1175319005cdb1a6e83af
lconeill <luke.c.oneill@gmail.com>
2015-10-04 18:57:54 -0400

    LifeSlider Logic
    Life slider will now increase/decrease by a set amount for every
    correct/incorrect match made

--- Assets/Scripts/LifeSlider.cs
+++ Assets/Scripts/LifeSlider.cs
@@ -5,19 +5,17 @@ using UnityEngine.UI;
 public class LifeSlider : MonoBehaviour
 {
 
-    public float lifeTime = 10;
-    public bool increaseWrongMatch = false;
+    public float lifeTime = 20;                     // How much seconds it takes the life slider to reach zero
+    public int matchSliderAmount = 1;               // The amount to increase/decrease the life slider for correct/incorrect matches
 
-    private Slider lifeSlider;
-    private WheelRotation wheelRotation;
+    private Slider lifeSlider;                      // The life slider GameObject
+    private WheelRotation wheelRotation;            // The wheel rotation GameObject
 
-    private float timeKeeper;
-    private int correctMatches;
-    private int previousCorrectMatches;
-    private int incorrectMatches;
-    private int previousIncorrectMatches;
+    private float timeKeeper;                       // Counter used to increase/decrease the life slider value
+    private int previousCorrectMatches;             // The number of correct matches of the last rendered frame
+    private int previousIncorrectMatches;           // The number of incorrect matches of the last rendered frame
 
-    // Use this for initialization
+    // Find references to the GameObjects
     void Start()
     {
         GameObject temp = GameObject.Find("lifeSlider");
@@ -26,30 +24,29 @@ public class LifeSlider : MonoBehaviour
         if (temp != null) { lifeSlider = temp.GetComponent<Slider>();}
         if (lifeSlider != null) { lifeSlider.value = 0.5f; }
 
-        if (temp_1 != null) { wheelRotation = temp.GetComponent<WheelRotation>(); }
+        if (temp_1 != null) { wheelRotation = temp_1.GetComponent<WheelRotation>(); }
         if (wheelRotation != null) 
-        { 
-            correctMatches = wheelRotation.match_count;
-            previousCorrectMatches = correctMatches;
-            incorrectMatches = wheelRotation.mismatched_count;
-            previousIncorrectMatches = incorrectMatches;
+        {
+            previousCorrectMatches = wheelRotation.match_count;
+            previousIncorrectMatches = wheelRotation.mismatched_count;
         }
     }
 
-    // Update is called once per frame
+    // Constantly decrease the value of life slider and end game when value is 1
+    // Increase/decrease the value based on whether a correct/incorrect match was made
     void Update()
     {
         timeKeeper += Time.deltaTime;
         lifeSlider.value = (lifeTime - timeKeeper) / lifeTime;
 
-        if (correctMatches > previousCorrectMatches)
+        if (wheelRotation.match_count > previousCorrectMatches)
         {
-            timeKeeper -= 5;
+            timeKeeper = Mathf.Clamp(timeKeeper - matchSliderAmount, 0, lifeTime);
         }
 
-        if (incorrectMatches > previousIncorrectMatches)
+        if (wheelRotation.mismatched_count > previousIncorrectMatches)
         {
-            timeKeeper += 5;
+            timeKeeper += matchSliderAmount;
         }
 
         if (timeKeeper >= lifeTime)
@@ -57,15 +54,17 @@ public class LifeSlider : MonoBehaviour
             Debug.Log("Game Over Sucka");
         }
 
-        previousIncorrectMatches = correctMatches;
-        previousIncorrectMatches = incorrectMatches;
+        previousCorrectMatches = wheelRotation.match_count;
+        previousIncorrectMatches = wheelRotation.mismatched_count;
     }
 
+    // Used to decrease the slider when a bad reward from the slot game is acquired
     public void decreaseSlider()
     {
         timeKeeper += 5;
     }
 
+    // Used to injcrease the slider when a good reward from the slot game is acquired
     public void increaseSlider()
     {
         timeKeeper -= 5;
