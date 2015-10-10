67dc27b55aa04c7cfe008f30d7f50d3e8504b55b
Jason Coelho <jason.coelho@me.com>
2015-10-09 21:15:44 -0400

    Game Over
    Added the Functionality to Restart the game and return to main menu.
    Commented code.

--- Assets/Scripts/GUI/ShowPanels.cs
+++ Assets/Scripts/GUI/ShowPanels.cs
@@ -7,7 +7,7 @@ public class ShowPanels : MonoBehaviour {
 	public GameObject optionsTint;							//Store a reference to the Game Object OptionsTint 
 	public GameObject menuPanel;							//Store a reference to the Game Object MenuPanel 
 	public GameObject pausePanel;							//Store a reference to the Game Object PausePanel 
-
+	public GameObject game_over_panel;
 
 	//Call this function to activate and display the Options panel during the main menu
 	public void ShowOptionsPanel()
@@ -28,11 +28,27 @@ public class ShowPanels : MonoBehaviour {
 	{
 		menuPanel.SetActive (true);
 	}
+	
+	// Call this function when the player loses 
+	// and show the Gameover UI.
+	
+	public void ShowGameOver()
+	{
+		game_over_panel.SetActive (true);
+	}
 
 	//Call this function to deactivate and hide the main menu panel during the main menu
 	public void HideMenu()
 	{
 		menuPanel.SetActive (false);
+	}
+	
+	// Call this function to hide the
+	// Game over UI
+	
+	public void HideGameOver()
+	{
+		game_over_panel.SetActive (false);
 	}
 	
 	//Call this function to activate and display the Pause panel during game play
