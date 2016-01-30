460e9d3c7eafdecfc16be80771960dad1a59b9d7
Jason Coelho <jason.coelho@me.com>
2016-01-29 22:14:21 -0500

    Giftiz progress
    Started integrating giftiz

---
+++ Assets/Scripts/GiftizController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GiftizController : MonoBehaviour 
+{
+	
+	public Sprite Naked, Normal, Gifted, Warning;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		ChangeGiftizButtonTexture();
+	}
+	
+	public void ChangeGiftizButtonTexture()
+	{
+		SpriteRenderer button = gameObject.GetComponent<Button>();
+		
+		switch(GiftizBinding.giftizButtonState)
+		{
+			case GiftizBinding.GiftizButtonState.Invisible : button.sprite = Naked; break;
+			case GiftizBinding.GiftizButtonState.Naked : button.sprite = Normal; break;
+			case GiftizBinding.GiftizButtonState.Badge : button.sprite = Gifted; break;
+			case GiftizBinding.GiftizButtonState.Warning : button.sprite = Warning; break;
+		}
+	}
+	
+}
